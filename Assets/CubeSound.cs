50c469b868662c3893be7d970ecc9fc591cf158d
Siann55 <takodo_exword22@yahoo.co.jp>
2019-01-08 19:52:57 +0900

    Lesson7-GoForwardUnitychanの課題提出

---
+++ Assets/CubeSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "GroundTag" || other.gameObject.tag == "BlockTag")
+        {
+            GameObject.Find("CubeGenerator").GetComponent<AudioSource>().Play();
+        }
+    }
+}
